37a817e99b257cc6fc7719e1e93aca81733271ba
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-12-13 18:49:01 +0100

    cd..

--- app/src/main/java/com/zpi/jogasimple/MainActivity.kt
+++ app/src/main/java/com/zpi/jogasimple/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.zpi.jogasimple
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +13,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mainImageView.setOnClickListener{
-            Toast.makeText(this,"Kliknięto w kwiat lotosu...",Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this,"Kliknięto w kwiat lotosu...",Toast.LENGTH_SHORT).show()
+            Snackbar.make(mainView,"Kliknięto w kwiat lotosu...",Snackbar.LENGTH_SHORT).show()
+        }
+
+        excerciseButton.setOnClickListener{
+            val excerciseIntent = Intent(this,ExcerciseActivity::class.java)
+            startActivity(excerciseIntent)
         }
     }
 }

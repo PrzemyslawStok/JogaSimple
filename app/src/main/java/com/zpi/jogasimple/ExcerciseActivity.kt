40a0d70f0fce8c4b9a486b069cc779ea9bbe8d92
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-12-15 18:18:47 +0100

    cd..

--- app/src/main/java/com/zpi/jogasimple/ExcerciseActivity.kt
+++ app/src/main/java/com/zpi/jogasimple/ExcerciseActivity.kt
@@ -2,10 +2,20 @@ package com.zpi.jogasimple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class ExcerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_excercise)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==android.R.id.home)
+            finish()
+
+        return super.onOptionsItemSelected(item)
     }
 }
